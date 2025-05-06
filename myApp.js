7f597e7f3963aff44f97e18fc17e11d38d917e52
montrell43 <tmerriweather@persevernow.org>
2025-05-06 10:07:32 -0500

    New commit

--- myApp.js
+++ myApp.js
@@ -32,18 +32,18 @@ app.use("/public", express.static(__dirname + "/public"));
 
 // ----------------CHALLENGE 5----------------
 // Serve JSON on a Specific Route
-// app.get("/json", (req, res) => {
-//     res.json({
-//         "message": "Hello json"
-//     });
-// });
-app.get('/json', (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    message = message.toUpperCase();
-  }
-  res.json({ message });
+app.get("/json", (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
 });
+// app.get('/json', (req, res) => {
+//   let message = "Hello json";
+//   if (process.env.MESSAGE_STYLE === 'uppercase') {
+//     message = message.toUpperCase();
+//   }
+//   res.json({ message });
+// });
 
 // ----------------CHALLENGE 6----------------
 // Use the .env File
