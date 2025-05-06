befdd0087fa752613e55a602c677fa72d0d9ebd5
montrell43 <tmerriweather@persevernow.org>
2025-05-06 09:59:30 -0500

    New commit

--- myApp.js
+++ myApp.js
@@ -32,10 +32,17 @@ app.use("/public", express.static(__dirname + "/public"));
 
 // ----------------CHALLENGE 5----------------
 // Serve JSON on a Specific Route
-app.get("/json", (req, res) => {
-    res.json({
-        "message": "Hello json"
-    });
+// app.get("/json", (req, res) => {
+//     res.json({
+//         "message": "Hello json"
+//     });
+// });
+app.get('/json', (req, res) => {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
 });
 
 // ----------------CHALLENGE 6----------------
